aa6dc9512d5e8df027a82ebc529a68ad71024202
Joofer <75534341+jooferj@users.noreply.github.com>
2026-03-08 20:12:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamburger.classList.toggle('toggle');
+});
